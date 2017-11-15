d73fcbb7a596f172ccbec8255700f269579e8c0e
wudi96519 <33290122+wudi96519@users.noreply.github.com>
2017-11-16 04:06:44 +0800

    Update controlCenter.c

--- usr/controlCenter.c
+++ usr/controlCenter.c
@@ -7,11 +7,6 @@
 #define UP_OR_DOWN_DECIDE_VALUE 1000
 #define BAND 3
 
-/**
- * TODO:��ÿ���������书��
- *      �趨�ʵ�ȫ�ֱ���
- */
-
 int DISPLAY_CURVE=1;
 static Uint16 SampleTable[];
 Uint16 trigger_flag=0;
@@ -38,21 +33,21 @@ void all_Sys_Init()
 void get_trigger_index(void)
 {
     int i=0, minus=0,max=0,max_flag=0,zero_flag;
-    if(trigger_flag==1)                               //�����ش���
+    if(trigger_flag==1)//rising edge
         for(i=0;i<128;i++)
         {
             minus = SampleTable[i+1]-SampleTable[i];
             if(minus-UP_OR_DOWN_DECIDE_VALUE>0)
             {INDEX_TO_DRAW=i;break;}
         }
-    else if(trigger_flag==0)                                           //�½��ش���
+    else if(trigger_flag==0) //falling edge
         for(i=0;i<128;i++)
         {
             minus = SampleTable[i+1]-SampleTable[i];
             if(minus+UP_OR_DOWN_DECIDE_VALUE<0)
             {INDEX_TO_DRAW=i;break;}
         }
-    else if(trigger_flag==2)                 //�͵�ƽ����
+    else if(trigger_flag==2)                 //low value
     {
         zero_flag=0;
         for(i=0;i<128;i++)
@@ -68,7 +63,7 @@ void get_trigger_index(void)
                 }
             }
     }
-    else if(trigger_flag==3)                 //�ߵ�ƽ����
+    else if(trigger_flag==3)                 //high value
     {
         max=SampleTable[i]/64;
         for(i=0;i<128;i++)
@@ -96,7 +91,6 @@ void ISR_key1()
 void ISR_key2()
 {
     votage_Level+=votage_Level==5?0:1;
-//    lcd_Toast("KEY 2\0");
 }
 
 void ISR_key3()
@@ -106,31 +100,21 @@ void ISR_key3()
 
 void ISR_key4()
 {
-    //num_Of_index++;
     num_Of_index=(num_Of_index==15)?15:num_Of_index+1;
     sample_index=0;
 }
 
 void ISR_key5()
 {
-//    lcd_Toast("KEY 5\0");
     DISPLAY_CURVE=DISPLAY_CURVE==1?0:1;
     if(!DISPLAY_CURVE)
             {
-                Lcd_ClearTXT();
-                Lcd_ClearBMP();
-                lcd_PutStr(0, 0, "Ƶ��:\0");
-                DELAY_LOOP();
+               lcd_Show_Freq_Volt();
             }
 }
 
 void ISR_key6()
 {
-   // num_Of_index--;
-//    if(num_Of_index>16).
-    //.....
-
-    //warnnig
     num_Of_index=(num_Of_index==0)?0:num_Of_index-1;
     sample_index=0;
 }
@@ -143,14 +127,9 @@ void ISR_key7()
 void ISR_key8()
 {
     votage_Level-=votage_Level==(-5)?0:1;
-    //lcd_Toast("KEY 8\0");
 }
 
 void ISR_key9()
 {
     trigger_flag=3;
-//    static char num[2]={'0','\0'};
-//    num[0]++;
-//    lcd_Toast(num);
-////    lcd_Toast("KEY 9\0");
 }
