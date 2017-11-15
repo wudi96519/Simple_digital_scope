a73d9ea1f3027357fc2cef412e1ab212dc8ee3c4
wudi96519 <33290122+wudi96519@users.noreply.github.com>
2017-11-16 04:12:17 +0800

    Update adcint.h

--- usr/adcint.h
+++ usr/adcint.h
@@ -1,18 +1,12 @@
 #ifndef __ADCINT_H_
 #define __ADCINT_H_
 
-/**
- * TODO:
- *      ��lcd�Ĳ������ܷ����ڰ���ʱ����ʱADC��һ���رգ�������Ҫ��ͣ adc �ж�
- *      wudi����wenhaoд adc_STOP();
- */
+#define BUF_SIZE   256                              //采样的数组长度
 
-#define BUF_SIZE   256                              //���������鳤��
+extern void adc_Restart();                          //采样完成后ADC自动停止，手动重启，( adc_Over() == 0 )
+extern void adc_Init();                             //ADC初始化
+extern int  adc_Over();                             //采样完成后返回   1 ，
 
-extern void adc_Restart();                          //������ɺ�ADC�Զ�ֹͣ���ֶ�������( adc_Over() == 0 )
-extern void adc_Init();                             //ADC��ʼ��
-extern int  adc_Over();                             //������ɺ󷵻�   1 ��
-
-static Uint16 SampleTable[];
+extern Uint16 SampleTable[];
 
 #endif
